37508e8c8ac016bf073e3f708f1063a1d1d2f8ca
appleresoundtheapple-tech <appleresoundtheapple@gmail.com>
2026-09-24 21:14:16 -0700

    Update 123.lua

--- 123.lua
+++ 123.lua
@@ -38,7 +38,7 @@ local fovConn = nil
 local speedConn = nil
 local monsterNotifyConn = nil
 
-local monsterNames = {"RushMoving", "AmbushMoving", "Eyes", "CeaseMoving", "Specimen 8", "A60", "Frostbite", "Rebound", "RipperMoving", "Silence", "Shocker"}
+local monsterNames = {"RushCounterPart", "AmbushMoving", "Eyes", "CeaseMoving", "Specimen 8", "A60", "Frostbite", "Rebound", "Death", "Silence", "Shocker"}
 
 local itemNames = {"Crucifix", "Flashlight", "Lighter", "Lockpick", "SkeletonKey", "Battery", "Vitamins", "Smoothie", "Candle", "Bandage", "Books"}
 
